79af6d1e578c8c5560ca494e6f3a8e7c57343089
Sven Ruppert <sven.ruppert@gmail.com>
2017-03-28 08:20:34 +0200

    added M02

---
+++ mutationtesting/src/main/java/org/rapidpm/event/mutationtesting/m02/M02.java
@@ -0,0 +1,28 @@
+package org.rapidpm.event.mutationtesting.m02;
+
+/**
+ * Copyright (C) 2017 RapidPM - Sven Ruppert
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by Sven Ruppert - RapidPM - Team on 28.03.17.
+ */
+public class M02 {
+
+  public int doWork(int a, int b) {
+    if (a < 2) {
+      return (a + b) * -1;
+    } else {
+      return a + b;
+    }
+  }
+
+
+}
